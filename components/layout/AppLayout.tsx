58855632d9df97d2a992591cb51cc1420114644b
Daniel Panazzolo da Rosa <panazzolo100@gmail.com>
2026-07-23 16:01:57 -0400

    Corrigir geração do Prisma Client no deploy

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -1,5 +1,5 @@
 import { Sidebar } from "./Sidebar";
-import { Topbar } from "./Topbar";
+import { Topbar } from "./TopBar";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
